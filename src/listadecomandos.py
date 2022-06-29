2dabbec2a03c99844fc72d6cd223f6873aab51f1
Brabec <lu_brabec@hotmail.com>
2022-06-29 20:12:05 -0300

    Organizando a casa
    Inserida estrutura do projeto

--- src/listadecomandos.py
+++ src/listadecomandos.py
@@ -1,6 +1,2 @@
 lista = [".xoxo", ".eitaman",".rapaz", ".irra", ".yamete", ".kudasai", ".posto7", ".dejavu", ".cavalo", ".merda", ".cadeirudo", ".trabaia", ".sistema", ".uepa", ".epa", ".pauquebrando", ".briga", ".eitaporra", ".bora", ".horadoshow", ".cademeupau", ".moreno", ".marilene", ".naoconsegue", ".taz", ".gonorreia", ".calma", ".javaiboltz", ".bicho", ".piruleta",".fdp", ".puta", ".bioca",".chucky",".detras",".vainao",".testa",".pedemesa", ".mickey",".weedzao",".partido",".doisovim",".treinar",".alarme",".fantasias",".fudido",".sextacheira",".aloispetor"]
 lista = '\n'.join(lista)
-
-
-
-
--- src/webserver.py
+++ src/webserver.py
@@ -2,20 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from threading import Thread
 from os import *
 
-
-
 app = Flask('')
 app.config['UPLOAD_PATH'] = '/'
 
 @app.route('/', methods=['POST','GET'])
 def home():
   return render_template("form.html")
-
-def upload_file():
-    uploaded_file = request.files['file']
-    uploaded_file.save(uploaded_file.filename)
-    
-    return redirect(url_for('form.html'))
     
 def run():
   app.run(host = '0.0.0.0', port = 8080)
@@ -23,3 +15,10 @@ def run():
 def keep_alive():
   t = Thread(target = run)
   t.start()
+
+""" @TODO: wip """
+def upload_file():
+    uploaded_file = request.files['file']
+    uploaded_file.save(uploaded_file.filename)
+    
+    return redirect(url_for('form.html'))

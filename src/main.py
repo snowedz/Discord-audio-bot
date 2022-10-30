2cf6192eb78908eb0d9cc163f0222ad2fc91067f
Brabec <lu_brabec@hotmail.com>
2022-10-29 21:23:37 -0300

    Atualização da Estrutura do Projeto

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import time
 import json
 import discord
-from discord.ext import commands
+from discord.ext import commands as c
 from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
 from webserver import keep_alive
@@ -10,10 +10,10 @@ from webserver import keep_alive
 load_dotenv()
 
 JSON_FILE = './src/structure.json'
-jsonFile = open(JSON_FILE)
-jsonData = json.load(jsonFile)
+with open(JSON_FILE, encoding='utf8') as jsonFile:
+    jsonData = json.load(jsonFile)
 
-client = commands.Bot(command_prefix='.')
+client = c.Bot(command_prefix='.')
 
 # return command list in alphabetical order
 def getCommands():
@@ -32,34 +32,34 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('.'):
-        if message.content.startswith('.help'):
-            embedVar = discord.Embed(title="Lista de comandos", color=0x00ff00)
-            embedVar.add_field(name="Lista", value=getCommands(), inline=False)
-            await message.channel.send(embed=embedVar)
-            return
 
-        discordCommand = message.content
-        if discordCommand in jsonData:
-            jsonObject = jsonData[discordCommand]
+    if message.content == '.help':
+        embedVar = discord.Embed(title="Lista de comandos", color=0x00ff00)
+        embedVar.add_field(name="Lista", value=getCommands(), inline=False)
+        await message.channel.send(embed=embedVar)
+        return
 
-            if jsonObject['image']:
-                await message.channel.send(jsonObject['image'])
+    discordCommand = message.content
+    if discordCommand in jsonData:
+        jsonObject = jsonData[discordCommand]
 
-            if jsonObject['phrase']:
-                await message.channel.send(jsonObject['phrase'])
+        if jsonObject['image']:
+            await message.channel.send(jsonObject['image'])
 
-            if jsonObject['audio']:
-                channel = message.author.voice.channel
-                voice = await channel.connect()
-                source = FFmpegPCMAudio(jsonObject['audio'])
-                voice.play(source)
-                # Is this really required? Can't be async?
-                time.sleep(jsonObject['duration'])
-                await voice.disconnect()
+        if jsonObject['phrase']:
+            await message.channel.send(jsonObject['phrase'])
 
-        else:
-            await message.channel.send('Digite .help para ver a lista de comandos')
+        if jsonObject['audio']:
+            channel = message.author.voice.channel
+            voice = await channel.connect()
+            source = FFmpegPCMAudio(jsonObject['audio'])
+            voice.play(source)
+            # Is this really required? Can't be async?
+            time.sleep(jsonObject['duration'])
+            await voice.disconnect()
+
+    else:
+        await message.channel.send('Digite .help para ver a lista de comandos')
 
 keep_alive()
 client.run(os.getenv('DISCORD_TOKEN'))
